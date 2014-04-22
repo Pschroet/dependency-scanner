76fff713311bdc82411e07a769b5a6eba9b072ce
Pschroet <pschroet@inf.fu-berlin.de>
2014-04-22 21:51:10 +0200

    implemented Java support

--- Scanner.rb
+++ Scanner.rb
@@ -29,6 +29,34 @@ def scanCAndCPP(file)
 	f.close()
 end
 
+def scanJava(file)
+	#open the file...
+	f = File.open(file, "r")
+	#and read all of it's lines
+	lines = File.readlines(file)
+	#counter to show the number of the current line
+	counter = 1
+	#show if the current file has any dependencies
+	dependencyArray = Array.new()
+	lines.each{
+		|i|
+		#remove newlines and make sure the encoding does not cause problems
+		line = i.chomp.force_encoding("ISO-8859-1").encode("utf-8", replace=nil)
+		#if it matches the style of an import in Java
+		if(line.start_with?("import"))
+			dependency = line.sub("import", "").sub(";", "").strip()
+			dependencyArray.push(dependency)
+			puts "\t-> line " + counter.to_s + " imports " + dependency
+		end
+		counter+=1
+	}
+	if(dependencyArray.length() == 0)
+		puts "\t-> this file has no dependencies"
+	end
+	$projectDependencies[file] = dependencyArray
+	f.close()
+end
+
 def checkDependencies(directory)
 	#if the passed directory exists
 	if(Dir.exist?(directory))
@@ -45,14 +73,18 @@ def checkDependencies(directory)
 				if(File.readable?(input))
 					#if it is a header for C or C++...
 					if(File.extname(input) == ".h" or File.extname(input) == ".hpp")
-							#then scan it for it's dependencies
-							puts "Scanning dependencies of file " + input
-							scanCAndCPP(input)
+						#then scan it for it's dependencies
+						puts "Scanning dependencies of file " + input
+						scanCAndCPP(input)
 					#or a C or C++ source code file
 					elsif(File.extname(input) == ".c" or File.extname(input) == ".cpp")
 						#then scan it for it's dependencies
 						puts "Scanning dependencies of file " + input
 						scanCAndCPP(input)
+					elsif(File.extname(input) == ".java")
+						#then scan it for it's dependencies
+						puts "Scanning dependencies of file " + input
+						scanJava(input)
 					else
 						#or skip it
 						puts "Skipping file " + input
